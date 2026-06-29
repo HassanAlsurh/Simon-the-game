e617d4596b5cefc7aa234b821b2c01a0c6b0d05e
HassanAlsurh <HassanAlsurh@gmail.com>
2026-06-29 12:53:10 +0300

    Added score and game mode divs in HTML and gameMode function + few variables in JS

--- js/app.js
+++ js/app.js
@@ -1,13 +1,42 @@
+/*-------------------------------- Constants --------------------------------*/
+/*-------------------------------- Variables --------------------------------*/
+let sequence = []
+let userInput = []
+let currentGameMode = 'Simon'
+/*------------------------ Cached Element References ------------------------*/
 
-const topLeft = document.querySelector('#top-left')
-const topRight = document.querySelector('#top-right')
-const bottomLeft = document.querySelector('#bottom-left')
-const bottomRight = document.querySelector('#bottom-right')
+const topLeft = document.querySelector('.top-left')
+const topRight = document.querySelector('.top-right')
+const bottomLeft = document.querySelector('.bottom-left')
+const bottomRight = document.querySelector('.bottom-right')
+
+const gameMode = document.querySelector('#gameMode')
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+topLeft.addEventListener('click', () => { console.log(topLeft) })
+topRight.addEventListener('click', () => { console.log(topRight) })
+bottomLeft.addEventListener('click', () => { console.log(bottomLeft) })
+bottomRight.addEventListener('click', () => { console.log(bottomRight) })
+
+//Set game mode
+gameMode.addEventListener('change', () => {
+    if (gameMode.checked) {
+        currentGameMode = 'Training'
+    } else {
+        currentGameMode = 'Simon'
+    }
+})
+
+/*-------------------------------- Functions --------------------------------*/
+// function nextSequence(params) {
+//     sequence.push()
+// }
+
+//Add a reset function... to reset the sequence when game mode is 'Simon',,,
 
 
 
-
-topLeft.addEventListener('click', ()=>{ console.log(topLeft)})
-topRight.addEventListener('click', ()=>{ console.log(topRight)})
-bottomLeft.addEventListener('click', ()=>{ console.log(bottomLeft)})
-bottomRight.addEventListener('click', ()=>{ console.log(bottomRight)})
+/*--------------------------------------------------------------------------------*/
+/*-------------------------------- Code GraveYard --------------------------------*/
+/*--------------------------------------------------------------------------------*/
