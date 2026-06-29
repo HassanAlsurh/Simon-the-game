1a5175ba579eb884f38d8edea5c8c725ffc25e62
HassanAlsurh <HassanAlsurh@gmail.com>
2026-06-29 10:15:05 +0300

    Simon circle and constant querySelectors for each color

---
+++ js/app.js
@@ -0,0 +1,13 @@
+
+const topLeft = document.querySelector('#top-left')
+const topRight = document.querySelector('#top-right')
+const bottomLeft = document.querySelector('#bottom-left')
+const bottomRight = document.querySelector('#bottom-right')
+
+
+
+
+topLeft.addEventListener('click', ()=>{ console.log(topLeft)})
+topRight.addEventListener('click', ()=>{ console.log(topRight)})
+bottomLeft.addEventListener('click', ()=>{ console.log(bottomLeft)})
+bottomRight.addEventListener('click', ()=>{ console.log(bottomRight)})
