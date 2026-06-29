0b155531677e85ac795e7cdeb570c0410674c321
HassanAlsurh <HassanAlsurh@gmail.com>
2026-06-29 15:33:22 +0300

    nextSequence fucntion & assign a class to the button when shown to user as a sequence

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 /*-------------------------------- Constants --------------------------------*/
+const finishCriteria = 25
 /*-------------------------------- Variables --------------------------------*/
 let sequence = []
 let userInput = []
@@ -29,11 +30,53 @@ gameMode.addEventListener('change', () => {
 })
 
 /*-------------------------------- Functions --------------------------------*/
-// function nextSequence(params) {
-//     sequence.push()
-// }
+nextSequence()
+
+//a function to change the class of the color to '.click' when the sequence gets generated
+function assignClickClass() {
+    // topLeft.classList += 'click'
+    // topLeft.id = 'banana'
+
+    for (let i = 0; i < sequence.length; i++) {
+        if (sequence[i] ===0){
+            topLeft.id = 'active'
+        }else if (sequence[i] ===1){
+            topRight.id = 'active'
+        }else if (sequence[i] ===2){
+            bottomLeft.id = 'active'
+        }else if (sequence[i] ===3){
+            bottomRight.id = 'active'
+        }
+        else{
+            //Delete after testing
+            alert('Something went wrong')
+        }
+    }
+}
+
+
+// function to add a new button/ to the sequence array...  || generates the same number time after the another alot
+function nextSequence() {
+    sequence.push(Math.floor(Math.random() * 4))
+    console.log(sequence)
+    assignClickClass()
+}
+
+
+//for testing
+topLeft.addEventListener('click', () => { 
+    nextSequence() 
+    console.log(sequence)
+ })
+
+
+
 
 //Add a reset function... to reset the sequence when game mode is 'Simon',,,
+
+//Add a function to be activated each time the user clicks the right button after the sequence,, if the sequence size is === to finishCriteria then user has won the game
+
+//function to update the current Score each time the user clicks the right button after the sequence /// and if the current Score is > than the Best score update the BEst score too.
 
 
 
